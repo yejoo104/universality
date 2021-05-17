0129b60af83cdf705101256cbda4d46b3244d52e
한예주 <yejoo_han@college.harvard.edu>
2021-05-17 22:39:21 +0900

    changed csv2json.py such that it converts to js files not json files, implemented checkfolderID in main

--- triallists/csv2json.py
+++ triallists/csv2json.py
@@ -11,12 +11,12 @@ def csv2json(csvFile, jsonFile):
             data.append([item[1] for item in list(row.items())[1:]])
     
     with open(jsonFile, 'w', encoding = 'utf-8') as writer:
-        writer.write(json.dumps(data))
+        writer.write("var triallist = " + json.dumps(data))
 
 if __name__ == "__main__":
     triallist_num = 100
     
     for i in range(1, triallist_num + 1):
         csvFile = "trialList_subject" + str(i) + ".csv"
-        jsonFile = "trialList_subject" + str(i) + ".json"
+        jsonFile = "trialList_subject" + str(i) + ".js"
         csv2json(csvFile, jsonFile)
