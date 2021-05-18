816fadee1a78638133e2d669e597c58ff8108cdb
한예주 <yejoo_han@college.harvard.edu>
2021-05-18 23:12:58 +0900

    created stims.js based on excel file, loaded into main

---
+++ input_files/stimsjs.py
@@ -0,0 +1,19 @@
+
+import pandas as pd
+import json
+
+def stimsjs():
+    df = pd.read_excel(r'select48.xlsx')
+    stims = {}
+    
+    for index, row in df.iterrows():
+        if row['Language'][:2] in stims:
+            stims[row['Language'][:2]].append(row['corpus_ID'][:-3] + 'wav')
+        else:
+            stims[row['Language'][:2]] = [row['corpus_ID'][:-3] + 'wav']
+    
+    with open('stims.js', 'w', encoding = 'utf-8') as writer:
+        writer.write("var stims = " + json.dumps(stims))
+
+if __name__ == "__main__":
+    stimsjs()
