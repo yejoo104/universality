a6402cfe97dcf62bb732a6ef5c564c6407d158df
한예주 <yejoo_han@college.harvard.edu>
2021-05-25 22:54:36 +0900

    wrote processdata.py and added write_data.php previouslly not added

---
+++ data/processdata.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+def processdata(datafile):
+    df = pd.read_csv(datafile)
+    df = df[df['task'] == 'response']
+    df = df[['data_type', 'L1', 'Excerpt1', 'L2', 'Excerpt2', 'Stimulus1', 'Stimulus2', 'time_elapsed', 'timestamp', 'rt', 'response']]
+    
+    for i, row in df.iterrows():
+        df.at[i, 'response'] = int(row['response']) + 1
+        
+    df = df.astype({'Excerpt1': int, 'Excerpt2': int})
+    df.index = np.arange(1, len(df) + 1)
+    df.to_csv(datafile[:-4] + "_cleaned.csv")
+
+if __name__ == "__main__":
+    processdata("subj_2_trialList_3_data.csv")
