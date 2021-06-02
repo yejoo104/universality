58c5088863b8900fb5b52c491c77141ddd7cebac
한예주 <yejoo_han@college.harvard.edu>
2021-06-02 23:01:18 +0900

    modified stimsjs.py to access different folders in soundFiles

--- input_files/stimsjs.py
+++ input_files/stimsjs.py
@@ -15,5 +15,20 @@ def stimsjs():
     with open('stims.js', 'w', encoding = 'utf-8') as writer:
         writer.write("var stims = " + json.dumps(stims))
 
+def stimsjsfolders(filename, folder):
+    df = pd.read_excel(r'select48.xlsx')
+    stims = {}
+    
+    for index, row in df.iterrows():
+        if row['Language'][:2] in stims:
+            stims[row['Language'][:2]].append("soundFiles/" + row['Language'] + "/" + folder + "/" + row['corpus_ID'][:-3] + "wav")
+        else:
+            stims[row['Language'][:2]] = ["soundFiles/" + row['Language'] + "/" + folder + "/" + row['corpus_ID'][:-3] + "wav"]
+    
+    with open(filename, 'w', encoding = 'utf-8') as writer:
+        writer.write("var stims = " + json.dumps(stims))
+
 if __name__ == "__main__":
     stimsjs()
+    stimsjsfolders("stims_normalized.js", "normalized")
+    stimsjsfolders("stims_lp400.js", "lp400")
